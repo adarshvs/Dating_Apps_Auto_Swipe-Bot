52ad71d6db5609ef20d1e1de87e358fc9e16c871
Adarsh V S <mannarakaladarsh@gmail.com>
2024-07-28 11:32:43 +0530

    Update bumble_swipe_bot.py
    To reload the page at random intervals and reduce the frequency of pressing the left button while also printing progress to the terminal

--- bumble_swipe_bot.py
+++ bumble_swipe_bot.py
@@ -5,29 +5,36 @@ from selenium.webdriver.common.keys import Keys
 import random
 from random import randint
 
-class BumbleBot():
+class TinderBot():
     def __init__(self):
         self.driver = webdriver.Chrome()
 
     def login(self):
-        self.driver.get('https://bumble.com/app')
+        self.driver.get('https://bumble.com/get-started')
 
         sleep(4)
-        print("Please log into your tinder account manually and provide necessary browser permissions.\n")
-        print("Log in with mobile number is preferable. \n")
+        print("Please log into your Tinder account manually and provide necessary browser permissions.\n")
+        print("Log in with a mobile number is preferable.\n")
         print("Don't forget to maximize your browser or this script won't work.\n")
-         
-        user_input = input("have you logged in ? yes or no : ")
-        
-        if user_input == 'yes':
-                moves = [Keys.LEFT, Keys.RIGHT, Keys.ESCAPE]
-                while True:
-                        sleep(randint(4,10))
-                        bot.driver.find_element(By.CSS_SELECTOR, "body").send_keys(random.choice(moves))
-                        #bot.driver.find_element_by_css_selector('body').send_keys(random.choice(moves))
+
+        user_input = input("Have you logged in? (yes or no): ")
+
+        if user_input.lower() == 'yes':
+            moves = [Keys.RIGHT, Keys.RIGHT, Keys.ESCAPE, Keys.RIGHT, Keys.RIGHT, Keys.LEFT]  # Reduce frequency of left button press
+            while True:
+                interval = randint(4, 15)
+                sleep(interval)
+                move = random.choice(moves)
+                self.driver.find_element(By.CSS_SELECTOR, "body").send_keys(move)
+                print(f"Action performed: {move}, sleeping for {interval} seconds.")
+                
+                # Randomly reload the page
+                if random.random() < 0.1:  # 10% chance to reload the page
+                    print("Reloading page...")
+                    self.driver.refresh()
+                    sleep(randint(10, 20))  # Additional sleep after reloading
         else:
-                print('sorry i cannot help you if you do not login')
+            print('Sorry, I cannot help you if you do not log in.')
 
-
-bot = BumbleBot()
+bot = TinderBot()
 bot.login()
